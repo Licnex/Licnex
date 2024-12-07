9833483068c30ff57b5a62358f9e1cafc8bcaa09
Momin Khan <mominravage@gmail.com>
2024-12-08 00:19:22 +0500

    Trying to make this work

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,22 @@
-import mdx from '@next/mdx';
-import createNextIntlPlugin from 'next-intl/plugin';
-
 const withMDX = mdx({
     extension: /\.mdx?$/,
     options: { },
-});
-
+  });
+  
 const withNextIntl = createNextIntlPlugin();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+  
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
     pageExtensions: ['ts', 'tsx', 'md', 'mdx'],
-};
-
-export default withNextIntl(withMDX(nextConfig));
+    target: 'serverless',
+    exportPathMap: async function (
+      defaultPathMap,
+      { dev, defaultLoad }
+    ) {
+      return {
+        '/': { page: '/' },
+      };
+    },
+  };
+  
+  export default withNextIntl(withMDX(nextConfig));
